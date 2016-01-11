941891b6fed4593be78b2ac759a97930b997a802
Mameaw <pinyapornster@gmail.com>
2016-01-11 13:14:06 -0500

    update 1

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', function(req,res){
 
 io.on('connection',function(socket){
 	console.log('a user connected');
+	socket.on('disconnect',function(){
+		console.log('a user disconnected');
+	});
+	socket.on('chat message',function(msg){
+		console.log('message: '+msg);
+		io.emit('chat message',msg);
+	});
+	
 });
 
 http.listen(3000,function(){
